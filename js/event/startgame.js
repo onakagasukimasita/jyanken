082aeedf0dcaa09c19557eed816826aae4ee0f03
takeo-yamanaka <98160206+takeo-yamanaka@users.noreply.github.com>
2023-01-13 21:53:51 +0900

    BGM再生を追記

--- js/event/startgame.js
+++ js/event/startgame.js
@@ -1,3 +1,4 @@
+//import ./js/uiux/sound.js
 
 function startgame(){
   
@@ -5,6 +6,15 @@ function startgame(){
   
   //画面移動する前にアニメーションを表示する
   
+  //BGMを開始する
+
+  //audioオブジェクトを生成する
+  bgmSet("./sound/bgm01.mp3");
+  
+  //ファイルを作ってその場所とファイル名を書く
+  bgmPlay();
+  
+  //ゲームを表示する
 　game_area = document.getElementById("game");
   game_area.style.display = "block";
   
