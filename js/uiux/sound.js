cd053426bc07e177235db17d6b15c5948aa160ac
takeo-yamanaka <98160206+takeo-yamanaka@users.noreply.github.com>
2023-01-13 12:43:01 +0900

    BGM移植

--- js/uiux/sound.js
+++ js/uiux/sound.js
@@ -1,4 +1,30 @@
+let BGM;
+
+//"./sound/bgm01.mp3"
+function bgmSet(filepath){
+    //audioオブジェクトを生成する
+    //ファイルを作ってその場所とファイル名を書く
+    BGM = new Audio(filepath);
+  
+    //audioオブジェクトの設定をする
+    BGM.loop = true;
+}
+  
 //1. ゲーム中ずっとBGMが流れる
+function bgmPlay(){
+    
+    //audioオブジェクトのファンクションを呼び出す
+    BGM.play();
+}
+
+function bgmPause(){
+    BGM.pause();
+}
+
+function bgmPause(){
+    BGM.pause();
+}
+
 //2. ボタンを押したら効果音
 //3. CPの手が決まるまでの間BGM
 //4. CPの手が決まった瞬間の効果音
