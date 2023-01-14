940cf7fb66ea2b2f1f4f23ce51c9917f14d57875
takeo-yamanaka <98160206+takeo-yamanaka@users.noreply.github.com>
2023-01-14 12:04:49 +0900

    スコア計算を追加

--- js/event/btn_jyanken.js
+++ js/event/btn_jyanken.js
@@ -1,19 +1,35 @@
-// require js/gamesystem/jyanken.js
+// requires js/gamesystem/jyanken.js
+// requires js/gamesystem/score.js
 let ELM_CP_RESULT;
 let ELM_KEKKA;
+const hand = { gu: "✊", ch: "✌️", pa: "✋" };
 
 //1. ユーザーが、自分の手を決めるボタンを押した際、の処理
 async function jyanken(my_hand) {
-    ELM_CP_RESULT = document.getElementById("cp_result");
     ELM_KEKKA = document.getElementById("game_result");
     ELM_KEKKA.innerHTML = "じゃ～んけ～ん...";
     //0. PCの手を確定する
     let cp_hand = getCpHand();
     //1. 結果を取得する
-    showResult(getKekka(cp_hand, my_hand), cp_hand);
+    let result = getKekka(cp_hand, my_hand);
+    showResult(result, cp_hand);
+    
+    //ichihachiの場合はaddScoreIchiHachi
+    switch (result){
+        case r_win:
+            addScore();
+            break;
+        case r_lose:
+            lostScore();
+            break;
+        case r_even:
+            //do nothing;
+    }
+    showScore();
 }
 
 function showResult(res, cp_hand) {
+    ELM_CP_RESULT = document.getElementById("cp_result");
     ELM_CP_RESULT.innerText = hand[cp_hand];
     ELM_KEKKA.innerText = res;
 }
