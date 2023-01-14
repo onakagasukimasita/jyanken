d15295a2bf6eeac02debb0a54a8d61a43555166a
takeo-yamanaka <98160206+takeo-yamanaka@users.noreply.github.com>
2023-01-14 12:33:01 +0900

    Update show_score.js

--- js/event/show_score.js
+++ js/event/show_score.js
@@ -1,3 +1,4 @@
+// requires js/gamesystem/score.js
 const area_my_score_id ="my_score";
 const area_cp_score_id ="cp_score";
 
@@ -6,5 +7,5 @@ function showScore(){
   let area_my_socre = document.getElementById(area_my_score_id);
   let area_cp_socre = document.getElementById(area_cp_score_id);
   area_my_socre.innerText = my_score;
-  area_cp_socre.innerText = pc_score;
+  area_cp_socre.innerText = cp_score;
 }
