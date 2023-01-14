92c7adb338840a4a7a588b7fa854d02277bba4f8
takeo-yamanaka <98160206+takeo-yamanaka@users.noreply.github.com>
2023-01-14 11:52:50 +0900

    表示関連をeventフォルダのファイルへ移動

--- js/gamesystem/jyanken.js
+++ js/gamesystem/jyanken.js
@@ -5,21 +5,6 @@ const r_even = "even";
 const r_win = "win";
 const r_lose = "lose";
 const hand = { gu: "✊", ch: "✌️", pa: "✋" };
-let ELM_CP_RESULT = document.getElementById("cp_result");
-let ELM_KEKKA = document.getElementById("game_result");
-let BGM1;
-
-
-//1. ユーザーが、自分の手を決めるボタンを押した際、の処理
-async function jyanken(my_hand) {
-    ELM_CP_RESULT = document.getElementById("cp_result");
-    ELM_KEKKA = document.getElementById("game_result");
-    ELM_KEKKA.innerHTML = "じゃ～んけ～ん...";
-    //0. PCの手を確定する
-    let cp_hand = getCpHand();
-    //1. 結果を取得する
-    showResult(getKekka(cp_hand, my_hand), cp_hand);
-}
 
 //2. CPが、自分の手を決める処理
 function getCpHand() {
@@ -89,7 +74,3 @@ function getRandomlyZeroToTwo() {
     return Math.floor(Math.random() * 3);
 }
 
-function showResult(res, cp_hand) {
-    ELM_CP_RESULT.innerText = hand[cp_hand];
-    ELM_KEKKA.innerText = res;
-}
