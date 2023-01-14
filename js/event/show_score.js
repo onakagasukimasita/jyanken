88131d3f3f265361dbe8efacf524cc9bc6411365
takeo-yamanaka <98160206+takeo-yamanaka@users.noreply.github.com>
2023-01-14 12:31:51 +0900

    Update show_score.js

--- js/event/show_score.js
+++ js/event/show_score.js
@@ -3,8 +3,8 @@ const area_cp_score_id ="cp_score";
 
 //画面に表示する
 function showScore(){
-  area_my_socre = document.getElementById(area_my_score_id);
-  area_cp_socre = document.getElementById(area_cp_score_id);
+  let area_my_socre = document.getElementById(area_my_score_id);
+  let area_cp_socre = document.getElementById(area_cp_score_id);
   area_my_socre.innerText = my_score;
-  area_pc_socre.innerText = pc_score;
+  area_cp_socre.innerText = pc_score;
 }
