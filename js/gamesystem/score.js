b7beaf03e1a6dc0cb2bcd80a81d14a02def89c61
takeo-yamanaka <98160206+takeo-yamanaka@users.noreply.github.com>
2023-01-14 12:05:40 +0900

    表示関連をEVENTフォルダの新設ファイルに移動

--- js/gamesystem/score.js
+++ js/gamesystem/score.js
@@ -2,8 +2,6 @@ let my_score;
 let cp_score;
 const winning_point = "1";
 const losing_point = "1";
-const area_my_score_id ="my_score";
-const area_cp_score_id ="cp_score";
 
 //初期状態
 function resetScore(){
@@ -29,11 +27,4 @@ function lostScore(){
   my_score = my_score - losing_point;
 }
 
-//画面に表示する
-function showScore(){
-  area_my_socre = document.getElementById(area_my_score_id);
-  area_cp_socre = document.getElementById(area_cp_score_id);
-  area_my_socre.innerText = my_score;
-  area_pc_socre.innerText = pc_score;
-}
  
