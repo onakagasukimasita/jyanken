62ee21a01d7cf7316016d2532f7bb4bc7f23fe4b
takeo-yamanaka <98160206+takeo-yamanaka@users.noreply.github.com>
2023-01-15 20:12:50 +0900

    cpを一旦削除

--- js/gamesystem/score.js
+++ js/gamesystem/score.js
@@ -1,12 +1,12 @@
 let my_score;
-let cp_score;
+//let cp_score;
 const winning_point = 1;
 const losing_point = 1;
 
 //初期状態
 function resetScore(){
   my_score = 0;
-  cp_score = 0;
+  //cp_score = 0;
 }
 
 //変更
