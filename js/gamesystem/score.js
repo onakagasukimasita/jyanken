1d35527e3ca93eebf8b4108670cc3c6c399567d9
takeo-yamanaka <98160206+takeo-yamanaka@users.noreply.github.com>
2023-01-15 21:07:27 +0900

    ハイスコア設定を追加

--- js/gamesystem/score.js
+++ js/gamesystem/score.js
@@ -1,4 +1,5 @@
 let my_score;
+let high_scores = [0,0,0,0,0];
 //let cp_score;
 const winning_point = 1;
 const losing_point = 1;
@@ -27,4 +28,16 @@ function lostScore(){
   my_score = my_score - losing_point;
 }
 
- 
+function setHighScore(score){
+  //配列から最小値を取得
+  let min = Math.min(...high_scores);
+  //スコアが配列内の最小値より大きいときだけ入れ替え
+  if(min<score){
+    //最小値を削除
+    high_scores = high_scores.filter(item => item !== min);
+    //新しい値を追加
+    high_scores.push(score);
+    //ソート
+    high_scores.sort(compareFunc(a, b) {return b - a});
+  }
+}
